47893ee291b0f84b00ae7c2a667be361c7cb8ec3
HiroshiOyamada <h5y1m141@gmail.com>
2016-01-27 12:17:19 +0900

    Jasmineのテストfailしてる箇所修正

--- spec/view/userLibrary_spec.js
+++ spec/view/userLibrary_spec.js
@@ -7,7 +7,7 @@ define([
         var fixture = "<div class='dataArea'><ul><script type='text/template' id='userLibraryView'><% _.each(items, function(item){ %><li>ファイル名：<%= item.path %></li><li>説明：<%= item.description %></li><% }); %></script></ul></div>",
             userLibraryView = new UserLibraryView();
         document.body.insertAdjacentHTML('afterbegin', fixture);
-        userLibraryView.render();
+        userLibraryView.fetch();
       });
       it('viewの描画される', function() {
         setTimeout("expect($('.dataArea > ul > li:nth-child(1)').text()).toEqual('ファイル名：sample01.png');", 2000);
