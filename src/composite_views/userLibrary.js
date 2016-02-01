829e1066d7f0bbd587ae5438ecfa994829b7ffa5
HiroshiOyamada <h5y1m141@gmail.com>
2016-02-01 14:48:07 +0900

    CompositeViewを利用して親子関係の構造を維持したViewのレンダリングを行えるように修正

---
+++ src/composite_views/userLibrary.js
@@ -0,0 +1,39 @@
+define([
+  'backbone',
+  'marionette',
+  'UserLibraryCollection',
+  'UserLibraryItemView',
+  'item_view/rowView'
+], function(Backbone, Marionette, UserLibraryCollection, UserLibraryItemView, RowView) {
+  var UserLibraryCompositeView;
+  UserLibraryCompositeView = Marionette.CompositeView.extend({
+    el: '#main',
+    childView: RowView,
+    childViewContainer: '.contents',
+    collection: UserLibraryCollection,
+    template: "#table-template",
+    events: {
+      'click #userLibrary': 'fetch'
+    },
+    initialize: function(options) {
+      this.collection = new UserLibraryCollection();
+    },
+    fetch: function() {
+      console.log('composit view load');
+      var _this = this;
+      this.collection.fetch({
+        dataType : 'json',
+        success: $.proxy(this.render, this)
+        // success : function(){
+        //   console.log(_this);
+        //   console.log($);
+        //   $.proxy(_this.render, _this);
+        // }
+      });
+    },
+    childViewOptions: function(model, index) {
+      console.log(index);
+    }
+  });
+  return UserLibraryCompositeView;
+});
--- src/userLibrary.js
+++ src/userLibrary.js
@@ -1,6 +1,8 @@
 requirejs([
   'UserLibraryCollectionView',
-  'collection_view/item'
-], function(UserLibraryCollectionView, ItemCollectionView) {
-  new UserLibraryCollectionView();
+  'collection_view/item',
+  'composite_views/userLibrary'
+], function(UserLibraryCollectionView, ItemCollectionView, UserLibraryCompositeViews) {
+  // new UserLibraryCollectionView();
+  new UserLibraryCompositeViews();
 });
