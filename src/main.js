0dcb95979808ccaab7fbefc55e1427868511b29b
HiroshiOyamada <h5y1m141@gmail.com>
2016-05-12 08:36:42 +0900

    クリックイベントサンプル箇所の表示不具合対応

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ define([
   'Router'
 ], function(Backbone, Marionette, TabCollectionView, Router) {
 	var SampleApp = Marionette.Application.extend({
-		setRootLayout: function () {
-      this.root = TabCollectionView;
+		setLayout: function () {
+      var tabCollectionVietw = new TabCollectionView();
+      tabCollectionVietw.render();
 		}
 	});
 	var MyApp = new SampleApp();  
 	MyApp.on('before:start', function () {
-		MyApp.setRootLayout();
+		MyApp.setLayout();
 	});
 	MyApp.on('start', function () {
     var router = new Router();
